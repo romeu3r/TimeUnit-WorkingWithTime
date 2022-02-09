0346795907259afed9be77431551ace6bd5df838
romeu3r <romeu3r@gmail.com>
2022-02-09 13:58:52 -0300

    Good solution time formatted.

--- src/program/Application.java
+++ src/program/Application.java
@@ -4,18 +4,24 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Application {
-    SimpleUtils utils = new SimpleUtils();
 
     public static void main(String[] args) {
         new Application().run();
     }
 
     public void run() {
+        SimpleUtils utils = new SimpleUtils();
         TimeUnit tus = TimeUnit.SECONDS;
         long temp1 = new Date().getTime();
         utils.threadSleep(1);
         long temp2 = new Date().getTime();
         long resposta = tus.convert(temp2 - temp1, TimeUnit.MILLISECONDS);
+//      ------------------------------
+
+        String timeFormated = utils.calcularDiferencaDeTempo(temp1, temp2);
+
         System.out.println("Time difference: " + resposta);
+        System.out.println("");
+        System.out.println("Tempo formatado: " + timeFormated);
     }
 }
--- src/program/SimpleUtils.java
+++ src/program/SimpleUtils.java
@@ -1,5 +1,7 @@
 package program;
 
+import java.util.concurrent.TimeUnit;
+
 public class SimpleUtils {
     public void threadSleep(int seconds) {
         try {
@@ -7,5 +9,28 @@ public class SimpleUtils {
         } catch (Exception e) {
             // Unnecessary
         }
+    }
+
+    public String calcularDiferencaDeTempo(long inicio, long tempFim) {
+        try {
+            long tempo = tempFim - inicio;
+            TimeUnit horas = TimeUnit.HOURS;
+            TimeUnit minutos = TimeUnit.MINUTES;
+            TimeUnit segundos = TimeUnit.SECONDS;
+            TimeUnit ms = TimeUnit.MILLISECONDS;
+            String sHora = formatData(String.valueOf(horas.convert(tempo, ms) % 60));
+            String sMin = formatData(String.valueOf(minutos.convert(tempo, ms) % 60));
+            String sSeg = formatData(String.valueOf(segundos.convert(tempo, ms) % 60));
+            return sHora + ":" + sMin + ":" + sSeg;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    private String formatData(String tempo) {
+        if (tempo.length() == 1)
+            return "0" + tempo;
+        else return tempo;
     }
 }
